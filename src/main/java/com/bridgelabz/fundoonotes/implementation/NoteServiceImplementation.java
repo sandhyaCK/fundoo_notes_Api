84d1c6658ffe815fb75e5c8a38fa0e17814669b0
sandhyaCK <csandhyait@gmail.com>
2020-02-13 19:28:07 +0530

    [sandhya].added service implemetation for note module

--- src/main/java/com/bridgelabz/fundoonotes/implementation/NoteServiceImplementation.java
+++ src/main/java/com/bridgelabz/fundoonotes/implementation/NoteServiceImplementation.java
@@ -1,18 +1,50 @@
 package com.bridgelabz.fundoonotes.implementation;
+import java.time.LocalDateTime;
 import java.util.*;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer.JwtConfigurer;
+import org.springframework.stereotype.Service;
 
 import com.bridgelabz.fundoonotes.dto.NoteDto;
 import com.bridgelabz.fundoonotes.dto.ReminderDto;
+import com.bridgelabz.fundoonotes.exception.UserException;
 import com.bridgelabz.fundoonotes.model.NoteData;
+import com.bridgelabz.fundoonotes.model.UserInformation;
+import com.bridgelabz.fundoonotes.repository.UserRepository;
 import com.bridgelabz.fundoonotes.service.NoteService;
+import com.bridgelabz.fundoonotes.utility.JwtGenerator;
 import com.bridgelabz.fundoonotes.dto.NoteUpdate;
-import com.bridgelabz.fundoonotes.dto.ReminderDto;
 
+@Service
 public class NoteServiceImplementation implements NoteService {
-
+@Autowired
+private JwtGenerator generate;
+@Autowired
+UserInformation userData;
+@Autowired
+UserRepository repository;
+@Autowired
+NoteData noteData;
 	@Override
 	public void createNote(NoteDto information, String token) {
-		// TODO Auto-generated method stub
+		try {
+		Long id=(Long)generate.parseJWT(token);
+		userData = repository.findUserById(id);
+		if(userData!=null) {
+			noteData.setCreatedDateAndTime(LocalDateTime.now());
+			noteData.setArchieved(false);
+			noteData.setColour("white");
+			noteData.setPinned(false);
+			noteData.setTrashed(false);
+		userData.getNote().add(noteData);		
+		NoteData data = 
+		}
+		}
+		catch(Exception e) {
+			throw new UserException("User doesnot exist with this id");
+		}
 		
 	}
 
